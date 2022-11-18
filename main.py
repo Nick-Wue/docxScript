1044f4300c46a19c214d1c3f31f8b8163206ff59
nickw <nickwuerflein@gmail.com>
2022-11-18 01:02:05 +0100

    smallstuff

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+
+import tkinter as tk
+from tkinter import filedialog
 from docx import Document
 import docxedit
 
 if __name__ == '__main__':
-    doc = Document("binder.weibliche_aerzte.docx")
+
+    root = tk.Tk()
+    root.withdraw()
+
+    path = filedialog.askopenfilename()
+    doc = Document(path)
     # adds <lb/> to any linebreak
     docxedit.replace_string(doc, "\n", "<lb/>\n ")
 
-
-    doc.save("test.docx")
+    doc.save("done_" + path)
